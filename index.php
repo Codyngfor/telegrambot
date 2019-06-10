6b3a694d85a6646c9f28af192f7cbe496c78f77b
Codyngfor <nikolya-ivanov-1999@bk.ru>
2019-06-10 16:48:20 +0600

    telegram bot version 1.1

--- index.php
+++ index.php
@@ -3,21 +3,35 @@
 
     $token = "875474339:AAG5Jix5QEX4kikIomawJDDViebeO68bilA";
     $bot = new \TelegramBot\Api\Client($token);
-    // команда для start
 
-    $bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
+    // команда для start
+    $bot->command('start', function ($message) use ($bot) 
+    {
+        $answer = 'Добро пожаловать!';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
     });
 
     // команда для помощи
-    $bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-    /help - вывод справки';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
+    $bot->command('help', function ($message) use ($bot) 
+    {
+        $answer = 'Команды:
+        /help - вывод справки';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+
+    $bot->command('hello', function ($message) use ($bot) 
+    {
+        $text = $message->getText();
+        $param = str_replace('/hello ', '', $text);
+        $answer = 'Неизвестная команда';
+        if (!empty($param))
+        {
+            $answer = 'Привет, ' . $param;
+        }
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
 
 $bot->run();
 
-echo "helaskfj;g";
+
 ?>
