e62dd20573c1bb8af6104e2fcc03b5bf9c4b16b0
Codyngfor <nikolya-ivanov-1999@bk.ru>
2019-06-11 11:12:31 +0600

    telegram bot version 1.1.6

--- index.php
+++ index.php
@@ -40,16 +40,16 @@
         $answer = 'Неизвестная команда';
         if (isset($param))
         {
-            $url = "https://api.openweathermap.org/data/2.5/weather?lat= 53.2143500&lon= 63.6246300&units=metric&appid=f2fddbc999a6344117a983a2ead391be&lang=ru";
+            $url = "https://api.openweathermap.org/data/2.5/weather?lat=53.2143500&lon=63.6246300&units=metric&appid=f2fddbc999a6344117a983a2ead391be&lang=ru";
 
             $contents = file_get_contents($url);
             $weather=json_decode($contents);
-            
+
             $temp_now=$weather->main->temp."°C";
             $today = date("j.m.Y, H:i:s");
-            $cityname = $weather->name;
+            // $cityname = $weather->name;
            
-            $answer = $today.", <br> ".$cityname.", <br>".$temp_now;
+            $answer = $today.",  ".$cityname.", ".$temp_now;
         }
         $bot->sendMessage($message->getChat()->getId(), $answer);
     });
