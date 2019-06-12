33effb6299bdd6eeb01e302be8c3d77ad54f76cc
Codyngfor <nikolya-ivanov-1999@bk.ru>
2019-06-12 11:31:53 +0600

    telegram bot version 1.2.3

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     require("vendor/autoload.php");
-    $json = require("current.city.list.json");
 
     $token = "875474339:AAG5Jix5QEX4kikIomawJDDViebeO68bilA";
     $bot = new \TelegramBot\Api\Client($token);
@@ -38,10 +37,8 @@
 
     $bot->command('setmysity', function ($message) use ($bot) 
     {
-        // не нужный код
-        $answer = 'Write your sity, please....';
-        // if ( $answer === 'Write your sity, please....') $success = true;
 
+        $answer = 'Write your sity, please....';
         // вывод сообщения боту 
         $bot->sendMessage($message->getChat()->getId(), $answer);
 
@@ -90,32 +87,33 @@
 
 
 
-    $bot->command('weather', function ($message) use ($bot) 
-    {
-        $text = $message->getText();
-        $param = str_replace('/weather ', '', $text);
-        $answer = 'Неизвестная команда';
-        if (isset($param))
-        {
-            $url = "https://api.openweathermap.org/data/2.5/weather?id=3073170&units=metric&appid=f2fddbc999a6344117a983a2ead391be&lang=ru";
+    // $bot->command('weather', function ($message) use ($bot) 
+    // {
+    //     $text = $message->getText();
+    //     $param = str_replace('/weather ', '', $text);
+    //     $answer = 'Неизвестная команда';
+    //     if (isset($param))
+    //     {
+    //         $url = "https://api.openweathermap.org/data/2.5/weather?id=3073170&units=metric&appid=f2fddbc999a6344117a983a2ead391be&lang=ru";
 
-            $contents = file_get_contents($url);
-            $weather=json_decode($contents);
+    //         $contents = file_get_contents($url);
+    //         $weather = json_decode($contents);
 
-            $temp_now=$weather->main->temp."°C";
-            $today = date("j.m.Y, H:i:s");
-            $cityname = $weather->name;
+    //         $temp_now=$weather->main->temp."°C";
+    //         $today = date("j.m.Y, H:i:s");
+    //         $cityname = $weather->name;
            
-            $answer = $today.",  ".$cityname.", ".$temp_now;
-        }
-        $bot->sendMessage($message->getChat()->getId(), $answer);
-    });
+    //         $answer = $today.",  ".$cityname.", ".$temp_now;
+    //     }
+    //     $bot->sendMessage($message->getChat()->getId(), $answer);
+    // });
 
 
     $bot->run();
 
     //functions
-    function translit($s) {
+    function translit($s)
+    {
         $s = (string) $s; // преобразуем в строковое значение
         $s = str_replace(array("\n", "\r"), " ", $s); // убираем перевод каретки
         $s = preg_replace("/\s+/", ' ', $s); // удаляем повторяющие пробелы
